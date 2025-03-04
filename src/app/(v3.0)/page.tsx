f335f355feaa8e6c8f14f68a9920688305409912
Mohan Arjunan <mohan.arjunan@finsurge.tech>
2025-03-04 16:47:29 +0530

    Revoked: TechStack
    On branch v3.0
    Changes to be committed:
      - modified:   src/app/(v3.0)/page.tsx
      - deleted:    src/components/v3/TechStack.tsx
    
    Signed-off-by: Mohan Arjunan <mohan.arjunan@finsurge.tech>

--- src/app/(v3.0)/page.tsx
+++ src/app/(v3.0)/page.tsx
@@ -1,20 +1,16 @@
 import React from 'react'
 import Hero from '@/components/v3/Hero'
 import Description from '@/components/v3/Description'
-import TechStack from '@/components/v3/TechStack'
 
 export default function V3Page() {
   return (
     <div>
       
       {/* Hero Section */}
-      {/* <Hero  /> */}
+      <Hero  />
 
       {/* About Section */}
-      {/* <Description /> */}
-
-      {/* Tech-Stack */}
-      <TechStack />
+      <Description />
 
     </div>
   )
