9471fa1c54418fcc1ce1521cc9f73eafee7dd2e5
Mohan Arjunan <mohan.arjunan@finsurge.ai>
2025-04-11 15:28:38 +0530

    Demo Fix

--- src/app/(v3.0)/layout.tsx
+++ src/app/(v3.0)/layout.tsx
@@ -5,6 +5,8 @@ interface V3LayoutProps {
   children: React.ReactNode
 }
 
+
+
 export default function V3Layout ({ children }: V3LayoutProps) {
   return (
     <div className="w-full h-auto overflow-y-scroll">
