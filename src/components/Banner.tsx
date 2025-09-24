6c549080d39f7b98c16f77a2ea2bcb55abdfb390
Mohan Arjunan <mohan.arjunan@hotmail.com>
2025-09-24 21:12:09 +0530

    Adding Banner
    On branch ~ `v4.0`
    Changes to be committed:
      - (M) Modified  : src/components/Banner.tsx
    
    Signed-off-by: Mohan Arjunan <mohan.arjunan@hotmail.com>

--- src/components/Banner.tsx
+++ src/components/Banner.tsx
@@ -4,6 +4,14 @@ const Banner = () => {
   return (
     <>
       <div className='relative w-full h-screen flex flex-col gap-3 justify-center items-center'>
+        <img src={"/assets/img/shape/grid.svg"} className='w-2/4 absolute right-0 top-0' />
+        {/* <img src={"/assets/img/shape/grid.svg"} className='w-2/4 absolute right-0 top-0' /> */}
+        {/* <img src={"/assets/img/shape/grid.svg"} className='w-2/4 absolute right-0 bottom-0 rotate-x-180' /> */}
+        {/* <img src={"/assets/img/shape/grid.svg"} className='w-2/4 absolute right-0 bottom-0 rotate-x-180' /> */}
+        <img src={"/assets/img/shape/grid.svg"} className='w-2/4 absolute left-0 top-0 rotate-y-180' />
+        {/* <img src={"/assets/img/shape/grid.svg"} className='w-2/4 absolute left-0 top-0 rotate-y-180' /> */}
+        {/* <img src={"/assets/img/shape/grid.svg"} className='w-2/4 absolute left-0 bottom-0 rotate-y-180 rotate-x-180' /> */}
+        {/* <img src={"/assets/img/shape/grid.svg"} className='w-2/4 absolute left-0 bottom-0 rotate-y-180 rotate-x-180' /> */}
         <div className='w-48 h-48 rounded-full border-5 border-gray-700 shadow overflow-hidden'>
           {/* <img src={'/assets/img/male-profile.jpg'} className='w-full' /> */}
         </div>
